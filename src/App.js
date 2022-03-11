f09d11f9960b79cab675f2648861ec1b849bb8a6
RuebenHales <halesrueben@gmail.com>
2022-03-10 20:16:15 -0500

    reminder-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import TextField from '@mui/material/TextField';
+import {React, useState } from 'react';
+import { Button } from '@mui/material';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [todoInput, setTodoInput] = useState('');//blank for input value,blank answer goes to todoInput
+
+
+  function addReminder(e) {
+    e.preventDefault();//stops page from reloading after submit and erasing
+    console.log(`you are trying to add a todo`);
+  }
+   return (
+    <div className='boca-code-reminder-app'
+         style={{
+           display: "flex", 
+           flexDirection:"column", 
+           justifyContent: "center",
+           alignItems: "center",
+            }}
+            >
+            <div>
+           <h1>Reminder App</h1>
+     <form>{/*if you want button clicked whenever you hit enter wrap it in form*/}
+     <TextField 
+     id="standard-basic" 
+     label="Add a reminder" 
+     value ={todoInput}//will save input in todoimput
+     onChange={(e) => setTodoInput(e.target.value)}
+  //onchange detects input change
+     style={{maxWidth:"300px", width: "90vx" }}//help the length shorter
+     />
+     <Button type="sumbit" variant="contained" onClick={addReminder}>+</Button>
+     </form>
+    </div>{/*mui coponents*/}
+    </div>//this div make the components and the h1 shorter
   );
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import { useState,useEffect } from "react"
+
+
+
+export default function TodoList() {
+ //use state for tasks and setTasks
+ const  [tasks,setTasks] = useState("")
+ //use effect to fetch from your API
+ 
+  return (
+    <h1>To Do</h1>
+  )
+}
